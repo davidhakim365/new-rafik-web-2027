4555367db732a90faee1969379ef518309cae112
InspirED6 <151472369+InspirED6@users.noreply.github.com>
2026-08-28 20:06:49 +0300

    Update CoursesErrors.cs

--- src/LearnMS.API/Features/Courses/CoursesErrors.cs
+++ src/LearnMS.API/Features/Courses/CoursesErrors.cs
@@ -6,6 +6,11 @@ public static class CoursesErrors
 {
     public static readonly ApiError NotFound = new ApiError("course/not-found", "Course not found", StatusCodes.Status404NotFound);
     public static readonly ApiError NotPublishable = new ApiError("course/not-publishable", "Course not publishable, please complete the course creation", StatusCodes.Status403Forbidden);
+    public static readonly ApiError AlreadyPurchased = new ApiError(
+        "course/already-purchased",
+        "Course already purchased",
+        StatusCodes.Status403Forbidden
+    );
     public static readonly ApiError WrongLevel = new ApiError(
         "course/wrong-level",
         "This course is not available for your grade",
